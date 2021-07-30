03f197ee8d9c2e04e8be9745063bee779a76f8b0
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-07-30 11:15:48 +0100

    new path

--- SpeechSplit/train_model.sh
+++ SpeechSplit/train_model.sh
@@ -1,9 +1,9 @@
 python main.py \
 	--num_iters=213000 \
-	--log_dir=run/logs/22k_waveglow \
-	--G_save_dir=run/models/22k_waveglow/G \
-	--P_save_dir=run/models/22k_waveglow/P \
+	--log_dir=run/logs/22k_waveglow_colab \
+	--G_save_dir=run/models/22k_waveglow_colab/G \
+	--P_save_dir=run/models/22k_waveglow_colab/P \
 	--train_G \
 	--train_P \
 	--log_step=100 \
-	--sample_dir=run/samples/22k_waveglow 
+	--sample_dir=run/samples/22k_waveglow_colab 
