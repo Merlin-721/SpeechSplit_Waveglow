5d70c75e2513c91cff060bd7a8068e95df235cd9
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-08-24 09:47:06 +0100

    works from args

--- show_mel.py
+++ show_mel.py
@@ -2,10 +2,12 @@ import torch
 import matplotlib.pyplot as plt
 from matplotlib.colors import Normalize
 import sys
+import numpy as np
+from pathlib import PurePath
 
 def plot_data(data, title=None):
 	fig, axes = plt.subplots(1,1)
-	axes.imshow(data.T, aspect='auto', origin='lower', 
+	axes.imshow(data.T, aspect='equal', origin='lower',
 		interpolation='none', norm=Normalize(-18,2))
 	if title is not None:
 		plt.title(title)
@@ -14,8 +16,16 @@ def plot_data(data, title=None):
 	
 if __name__ == '__main__':
 	path = sys.argv[1]
+	name = sys.argv[2]
 
-	with open(path, "rb") as file:
-		spec = torch.load(file)
+	path = PurePath(path)
 
-	plot_data(spec)
+	if path.suffix == '.pt':
+		with open(path, "rb") as file:
+			spec = torch.load(file)
+	elif path.suffix == '.npy':
+		spec = np.load(path)
+	else:
+		raise ValueError('Unknown file extension')
+
+	plot_data(spec, name)
