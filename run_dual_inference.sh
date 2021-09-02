1732a5c487cecd47526525001362f83dd3c5c17c
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-09-02 16:41:48 +0100

    remove P

--- run_dual_inference.sh
+++ run_dual_inference.sh
@@ -5,7 +5,6 @@ python dual_inference.py \
 	-output_name=test \
 	-speech_split_conf=SpeechSplit/run/models/Full_C_8_16_R_8_4_P_8_32/hparams.json \
 	-ss_g=SpeechSplit/run/models/Full_C_8_16_R_8_4_P_8_32/50000-G.ckpt \
-	-ss_p=SpeechSplit/run/models/Full_C_8_16_R_8_4_P_8_32/50000-P.ckpt \
 	-waveglow_model=Waveglow/models/waveglow_256channels_universal_v5.pt \
 	-waveglow_conf=Waveglow/config.json \
 	-sigma=1 \
