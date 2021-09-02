0c9d1fe9dcc87b89a7257fc8e1353a00af955bf7
ThorChadMerlin <merlinlindsay@hotmail.com>
2021-09-02 15:58:42 +0100

    new params

--- run_dual_inference.sh
+++ run_dual_inference.sh
@@ -1,11 +1,11 @@
 python dual_inference.py \
-	-source=/home/merlin/OneDrive/modules/individualProject/voiceChanger/Evaluation/Eval_Dataset_Reduced/Men \
-	-target=/home/merlin/OneDrive/modules/individualProject/voiceChanger/Evaluation/Eval_Dataset_Reduced/Woman_target/p297_008.wav \
-	-output_dir=/home/merlin/OneDrive/modules/individualProject/voiceChanger/Evaluation/Resemblyzer/audio_data/SpeechSplit_Waveglow/man_to_woman \
+	-source=/home/merlin/OneDrive/modules/individualProject/voiceChanger/Evaluation/Eval_Dataset_Reduced/Women \
+	-target=p270 \
+	-output_dir=Converted_wavs \
 	-output_name=test \
 	-speech_split_conf=SpeechSplit/run/models/Full_C_8_16_R_8_4_P_8_32/hparams.json \
 	-ss_g=SpeechSplit/run/models/Full_C_8_16_R_8_4_P_8_32/50000-G.ckpt \
 	-ss_p=SpeechSplit/run/models/Full_C_8_16_R_8_4_P_8_32/50000-P.ckpt \
 	-waveglow_model=Waveglow/models/waveglow_256channels_universal_v5.pt \
 	-waveglow_conf=Waveglow/config.json \
-	-sigma=0.6 \
+	-sigma=1 \
